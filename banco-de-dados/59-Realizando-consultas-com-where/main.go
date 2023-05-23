4e463c1ef090c55f31114cbf473dee90797b638b
Daniel <daniel.agostinho@hotmail.com>
2023-05-22 21:59:25 -0300

    feat: 59 Realizando consultas com where

---
+++ banco-de-dados/59-Realizando-consultas-com-where/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID    int `gorm:"primaryKey"`
+	Name  string
+	Price float64
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&Product{})
+
+	// create
+	//db.Create(&Product{
+	//	Name:  "Notebook",
+	//	Price: 1000.00,
+	//})
+
+	// crete in batch
+	//products := []Product{
+	//	{Name: "Notebook", Price: 1000.00},
+	//	{Name: "Mouse", Price: 50.00},
+	//	{Name: "Keyboard", Price: 100.00},
+	//}
+	//db.Create(products)
+
+	// select one
+	//var product Product
+	//db.First(&product, 1)
+	//fmt.Println(product)
+	//db.First(&product, "name = ?", "Mouse")
+	//fmt.Println(product)
+
+	//select all
+	//var products []Product
+	//db.Limit(2).Offset(0).Find(&products)
+	//for _, product := range products {
+	//	fmt.Println(product)
+	//}
+
+	// where
+	//var products []Product
+	//db.Where("price >?", 100).Find(&products)
+	//for _, product := range products {
+	//	fmt.Println(product)
+	//}
+
+	// Like
+	var products []Product
+	db.Where("name LIKE ?", "%note%").Find(&products)
+	for _, product := range products {
+		fmt.Println(product)
+	}
+}
