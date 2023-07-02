92b462b129d654f7c55b7860192efab50996b273
Daniel <daniel.agostinho@hotmail.com>
2023-07-02 16:48:01 -0300

    feat: Criando handler para criar produto

--- 09-api/cmd/server/main.go
+++ 09-api/cmd/server/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/DanielAgostinhoSilva/curso-go-expert/api/77-criando-arquivos-de-configuracao/configs"
+import (
+	"github.com/DanielAgostinhoSilva/curso-go-expert/09-api/internal/domain/model"
+	"github.com/DanielAgostinhoSilva/curso-go-expert/api/77-criando-arquivos-de-configuracao/configs"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
+)
 
 func main() {
-	config, _ := configs.LoadConfig("./cmd/server/.env")
-	println(config.DBDriver)
+	_, err := configs.LoadConfig("./cmd/server/.env")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&model.Product{}, &model.User{})
+
+	http.ListenAndServe(":8000", nil)
+
 }
---
+++ 09-api/internal/handler/product_handler.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/DanielAgostinhoSilva/curso-go-expert/09-api/internal/domain/model"
+	"github.com/DanielAgostinhoSilva/curso-go-expert/09-api/internal/dto"
+	"github.com/DanielAgostinhoSilva/curso-go-expert/09-api/internal/infrastructure/database"
+	"net/http"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductAdapter
+}
+
+func NewProductHandler(db database.ProductAdapter) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var productInput dto.ProductInput
+	err := json.NewDecoder(r.Body).Decode(&productInput)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product, err := model.NewProduct(productInput.Name, productInput.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.ProductDB.Save(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
