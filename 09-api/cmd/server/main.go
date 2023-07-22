241d3c9503a9a73c4e412dc0c630648faf343d7b
Daniel <daniel.agostinho@hotmail.com>
2023-07-22 14:42:58 -0300

    feat: Gerando primeira documentação

--- 09-api/cmd/server/main.go
+++ 09-api/cmd/server/main.go
@@ -14,6 +14,23 @@ import (
 	"net/http"
 )
 
+// @title           Go Expert Example API
+// @version         1.0
+// @description     Product API with authentication.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Test Test
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /
+// @securityDefinitions.apikey  ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
 	configs, err := configs.LoadConfig("./cmd/server/.env")
 	if err != nil {
