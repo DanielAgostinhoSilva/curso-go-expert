fceeacd007f0150782dafc79f16bbfd86d148cb5
Daniel <daniel.agostinho@hotmail.com>
2023-07-23 12:24:54 -0300

    feat: Problema simples de concorrência

---
+++ 10-multithrending/3-Concorrencia-no-mundo-ideal/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var number uint64 = 0
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		number++
+		w.Write([]byte(fmt.Sprintf("Você teve acesso a essa pagina %d vezes.", number)))
+	})
+	http.ListenAndServe(":3000", nil)
+}
