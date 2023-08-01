80eb7e28af64278c3a7c7a3c1153c7d0d5def6f9
Daniel <daniel.agostinho@hotmail.com>
2023-08-01 20:30:34 -0300

    feat: Criando consumidor na pratica

---
+++ 11-eventos/cmd/consumer/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/DanielAgostinhoSilva/curso-go-expert/11-eventos/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	msgs := make(chan amqp.Delivery)
+	go rabbitmq.Consumer(ch, msgs)
+	for msg := range msgs {
+		fmt.Println(string(msg.Body))
+		msg.Ack(false)
+	}
+}
