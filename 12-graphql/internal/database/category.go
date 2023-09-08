5b76fc29449f948bbc296de62a4b96e6a68e5b8d
Daniel <daniel.agostinho@hotmail.com>
2023-09-08 20:44:54 -0300

    feat: Fazendo queries de Category

--- 12-graphql/internal/database/category.go
+++ 12-graphql/internal/database/category.go
@@ -22,3 +22,20 @@ func (props *Category) Create(name string, description string) (Category, error)
 		id, name, description)
 	return Category{ID: id, Name: name, Description: description}, err
 }
+
+func (props *Category) FindAll() ([]Category, error) {
+	rows, err := props.db.Query("SELECT id, name, description FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var categories []Category
+	for rows.Next() {
+		var id, name, description string
+		if err := rows.Scan(&id, &name, &description); err != nil {
+			return nil, err
+		}
+		categories = append(categories, Category{ID: id, Name: name, Description: description})
+	}
+	return categories, err
+}
