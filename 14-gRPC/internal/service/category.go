9b5e40e6fceb481aeb38bbd4c5dac719efdb4153
Daniel <daniel.agostinho@hotmail.com>
2023-09-10 16:30:09 -0300

    feat: Implementando CreateCategory

---
+++ 14-gRPC/internal/service/category.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"context"
+	"github.com/DanielAgostinhoSilva/curso-go-expert/14-gRPC/internal/database"
+	"github.com/DanielAgostinhoSilva/curso-go-expert/14-gRPC/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (props *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := props.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+	categoryResponse := &pb.CategoryResponse{
+		Category: &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		},
+	}
+	
+	return categoryResponse, err
+}
